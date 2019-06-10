4cd1b9796b3356332218488d1d418f7983395209
Duminda <dumindags@gmail.com>
2019-06-10 18:37:59 +0530

    coded Default.js page & styled it

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 export default class Default extends Component {
   render() {
-    return <h4>Hello From Error Page</h4>;
+    return (
+      <Header title="404" styleClass="default-hero">
+        <h2 className="text-light text-uppercase">
+          you are in the wrong place
+        </h2>
+        <Link to="/" className="text-uppercase btn btn-secondary btn-lg mt-3">
+          return home
+        </Link>
+      </Header>
+    );
   }
 }
