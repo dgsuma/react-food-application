390866d3d14e3007751006e1c25ac18c7487284d
Duminda <dumindags@gmail.com>
2019-06-10 18:07:23 +0530

    coded Header component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 export default class Home extends Component {
   render() {
-    return <h4>Hello From Home Page</h4>;
+    return (
+      <Header title="amazing recipes">
+        <Link
+          to="recipes"
+          className="text-uppercase btn btn-secondary btn-lg mt-3"
+        >
+          search recipes
+        </Link>
+      </Header>
+    );
   }
 }
