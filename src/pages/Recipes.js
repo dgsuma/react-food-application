178521d61aacf6d007e621298c052680b7b00095
Duminda <dumindags@gmail.com>
2019-06-10 19:22:55 +0530

    created Search.js, Recipe.js & RecipeList.js components & coded Recipes.js page

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,7 +1,39 @@
 import React, { Component } from "react";
+import RecipeList from "../components/RecipeList";
+import Search from "../components/Search";
+import { recipeData } from "../data/tempList";
 
 export default class Recipes extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+    recipes: recipeData,
+    search: ""
+  };
+
+  handleChange = event => {
+    this.setState({
+      search: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+  };
+
   render() {
-    return <h4>Hello From Recipes Page</h4>;
+    // ReactFragment created <></> below
+    return (
+      <>
+        <Search
+          search={this.state.search}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        <RecipeList recipes={this.state.recipes} />
+      </>
+    );
   }
 }
