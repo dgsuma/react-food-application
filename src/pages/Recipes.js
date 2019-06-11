d7879fb92390529261710d27f2c83a2a1e4193e5
Duminda <dumindags@gmail.com>
2019-06-11 16:35:25 +0530

    coded Recipes.js page to get data dynamically using fetch API's,  AJAX request

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -6,12 +6,36 @@ import { recipeData } from "../data/tempList";
 export default class Recipes extends Component {
   constructor(props) {
     super(props);
+    this.getRecipes = this.getRecipes.bind(this);
   }
 
   state = {
+    // 3 dummy datas from tempList and put in 'recipes' array
     recipes: recipeData,
-    search: ""
+    search: "",
+    //  use below url to get actual data
+    url: `https://www.food2fork.com/api/search?key=${
+      process.env.REACT_APP_API_KEY
+    }`
   };
+
+  // uses fetch() API AJAX request to get actual data in json format
+  // and store them in 'recipes' array.
+  async getRecipes() {
+    try {
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
+      this.setState({
+        recipes: jsonData.recipes
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  componentDidMount() {
+    this.getRecipes();
+  }
 
   handleChange = event => {
     this.setState({
