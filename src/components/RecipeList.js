de3ff1bf8d8fe4999b66c491241bc99e47a438af
Duminda <dumindags@gmail.com>
2019-06-11 06:52:11 +0530

    coded RecipeList.js file

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -3,11 +3,25 @@ import Recipe from "./Recipe";
 
 export default class RecipeList extends Component {
   render() {
+    const { recipes } = this.props;
     return (
-      <div>
-        Hello From Recipe List
-        <Recipe />
-      </div>
+      // ReactFragment <></> used
+      <>
+        <div className="container py-5">
+          {/* title */}
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+              <h1 className="text-slanted">recipe list</h1>
+            </div>
+          </div>
+          {/* end of title */}
+          <div className="row">
+            {recipes.map(recipe => (
+              <Recipe key={recipe.recipe_id} recipe={recipe} />
+            ))}
+          </div>
+        </div>
+      </>
     );
   }
 }
